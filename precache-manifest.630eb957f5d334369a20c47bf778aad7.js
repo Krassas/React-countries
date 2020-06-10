05e2b5380c4d1fad17d1dee032cb750db73b0914
Meletis Krassas <meletiskrassas@outlook.com.gr>
2020-06-10 10:48:52 +0300

    Updates

---
+++ precache-manifest.630eb957f5d334369a20c47bf778aad7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94787501804562f1f286c29e2b8ebdc3",
+    "url": "/React-countries/index.html"
+  },
+  {
+    "revision": "b1b3713015ad240a883e",
+    "url": "/React-countries/static/css/main.99351663.chunk.css"
+  },
+  {
+    "revision": "df83f7cc2e07f85d3938",
+    "url": "/React-countries/static/js/2.c621627f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-countries/static/js/2.c621627f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1b3713015ad240a883e",
+    "url": "/React-countries/static/js/main.928add3b.chunk.js"
+  },
+  {
+    "revision": "3f9e29a5a3c8120baa69",
+    "url": "/React-countries/static/js/runtime-main.d48c572d.js"
+  }
+]);
